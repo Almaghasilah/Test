d9705bf79e3689f76d5e40d1731fba91a15d83de
Ahmed Almaghasilah <ahmed.almaghasilah@maine.edu>
2022-06-16 14:13:48 -0400

    adding function to document

---
+++ example1.py
@@ -0,0 +1,10 @@
+def func1(arg1, arg2):
+    """
+    This function take two arguments
+    :param arg1: some value
+    :param arg2: another value
+    return: 
+    """
+
+    arg1 = arg2
+    return arg1
